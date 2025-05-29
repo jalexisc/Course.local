5b337eec0dc3e082fec5d035bc7798a0f0fbbfe8
Jose Alexis Correa valencia <jalexiscv@gmail.com>
2025-05-29 11:51:14 -0500

    Actualización

--- source/conf.py
+++ source/conf.py
@@ -86,7 +86,7 @@ html_last_updated_fmt = '%b %d, %Y'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'static/logo.svg'
+html_logo = 'static/logo.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs. This file should be a Windows icon file (.ico) being 16x16 or 32x32
